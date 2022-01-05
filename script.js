216e84cf95a53efeb8d9ea50b978c07c500d7aa8
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-01-05 17:24:46 -0300

    feat: add function in products

---
+++ script.js
@@ -0,0 +1,18 @@
+var result = document.getElementById("result")
+
+const nextImage1 = () => {
+    var image1 = document.getElementById("image1")
+    result.innerHTML = `<img src="images/image-product-1-thumbnail.jpg">`
+}
+const nextImage2 = () => {
+    var image1 = document.getElementById("image1")
+    result.innerHTML = `<img src="images/image-product-2-thumbnail.jpg">`
+}
+const nextImage3 = () => {
+    var image1 = document.getElementById("image1")
+    result.innerHTML = `<img src="images/image-product-3-thumbnail.jpg" >`
+}
+const nextImage4 = () => {
+    var image1 = document.getElementById("image1")
+    result.innerHTML = `<img src="images/image-product-4-thumbnail.jpg" >`
+}
